199fa9153e0c5477820d1ae0f03b07cbbdf0c793
Nathália Benevides <122914216+nathbenevides@users.noreply.github.com>
2023-02-04 14:43:17 -0300

    Rename ex008.py to 8_conversão_de_medidas.py

--- ex008.py
+++
@@ -1,3 +0,0 @@
-# 8- Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
-m = float(input('Digite um numero : '))
-print('Analisando {} metros, seu valor em? \n km: {} \n hm: {} \n dam: {} \n dm: {} \n cm {} \n mm: {}'.format((m),(m*0.001), (m*0.01), (m*0.1), (m*10), (m*100), (m*1000)))
